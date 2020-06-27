ec7c8bc89aa6f58093ece2add1e0de77248d36ba
yassine <yassineyounsii@gmail.com>
2020-06-27 11:40:08 +0200

    errors Handling

--- src/main/java/com/example/javatest/UtilFunctions/Lists.java
+++ src/main/java/com/example/javatest/UtilFunctions/Lists.java
@@ -3,26 +3,34 @@ package com.example.javatest.UtilFunctions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lists {
+public abstract class Lists {
 
+    public static String EMPTYLISTERROR = "Couldn't do the partition, the list given is empty";
+    public static String NULLSIZEVALUE = "Couldn't do the partition, the size of the sub Lists required is null";
+    public static String SMALLLISTERROR = "Couldn't do the partition, the sub lists's size is bigger than the parent list's size";
 
-    public static List<List> partition(List<Integer> liste, int coef) throws Exception {
-        if (liste.size() > coef) {
+    public static List<List> partition(List<Integer> liste, int taille) throws Exception {
+        if (liste.isEmpty())
+            throw new Exception(EMPTYLISTERROR);
+        else if (taille == 0)
+            throw new Exception(NULLSIZEVALUE);
+        else if (liste.size() < taille)
+            throw new Exception(SMALLLISTERROR);
+        else {
             int index = 0;
-            List<List> result = new ArrayList<List>();
+            List<List> result = new ArrayList<>();
             List subList = new ArrayList<>();
             for (int nbr : liste) {
                 subList.add(nbr);
                 index++;
-                if (index % coef == 0 || index == liste.size()) {
+                if (index % taille == 0 || index == liste.size()) {
                     result.add(subList);
                     subList = new ArrayList<>();
                 }
 
             }
             return result;
-        } else
-            throw new Exception("Couldn't do the partition, the coef is bigger than the list's size");
+        }
     }
 
 
--- src/test/java/com/example/javatest/ListsTests.java
+++ src/test/java/com/example/javatest/ListsTests.java
@@ -2,6 +2,8 @@ package com.example.javatest;
 
 import com.example.javatest.UtilFunctions.Lists;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
@@ -12,26 +14,47 @@ import java.util.List;
 
 @SpringBootTest
 class ListsTests {
+    List<Integer> liste1;
+    List<Integer> liste2;
+    List<Integer> liste3;
+    List<Integer> emptyList;
+    @BeforeEach
+    void init() {
+        emptyList = Arrays.asList();
+        liste1 = Arrays.asList(1, 8, 9, 12, 3, 10);
+        liste2 = Arrays.asList(12, 81, 4, 5, 31, 17, 99);
+        liste3 = Arrays.asList(18, 93, 22, 74, 30, 21, 2, 72, 11, 57, 0);
+    }
 
     @Test()
     void partitionTest() throws Exception {
-        List<Integer> liste1 = Arrays.asList(1, 8, 9, 12, 3, 10);
-        List<Integer> liste2 = Arrays.asList(12, 81, 4, 5, 31, 17, 99);
-        List<Integer> liste3 = Arrays.asList(18, 93, 22, 74, 30, 21, 2, 72, 11, 57, 0);
         List<List> resultListe1_1 = Arrays.asList(Arrays.asList(1, 8), Arrays.asList(9, 12),
                 Arrays.asList(3, 10));
-        List<List> resultListe1_2 = Arrays.asList(Arrays.asList(1, 8, 9), Arrays.asList(12, 3, 10));
-        List<List> resultListe1_3 = Arrays.asList(Arrays.asList(1, 8, 9, 12), Arrays.asList(3, 10));
-        List<List> resultListe2_1 = Arrays.asList(Arrays.asList(12, 81), Arrays.asList(4, 5),
-                Arrays.asList(31, 17), Arrays.asList(99));
-        List<List> resultListe2_2 = Arrays.asList(Arrays.asList(12, 81, 4), Arrays.asList(5, 31, 17),
+        List<List> resultListe1_2 = Arrays.asList(Arrays.asList(1, 8, 9),
+                Arrays.asList(12, 3, 10));
+        List<List> resultListe1_3 = Arrays.asList(Arrays.asList(1, 8, 9, 12),
+                Arrays.asList(3, 10));
+        List<List> resultListe2_1 = Arrays.asList(Arrays.asList(12, 81),
+                Arrays.asList(4, 5),
+                Arrays.asList(31, 17),
                 Arrays.asList(99));
-        List<List> resultListe2_3 = Arrays.asList(Arrays.asList(12, 81, 4, 5), Arrays.asList(31, 17, 99));
-        List<List> resultListe3_1 = Arrays.asList(Arrays.asList(18, 93), Arrays.asList(22, 74),
-                Arrays.asList(30, 21), Arrays.asList(2, 72), Arrays.asList(11, 57), Arrays.asList(0));
-        List<List> resultListe3_2 = Arrays.asList(Arrays.asList(18, 93, 22), Arrays.asList(74, 30, 21),
-                Arrays.asList(2, 72, 11), Arrays.asList(57, 0));
-        List<List> resultListe3_3 = Arrays.asList(Arrays.asList(18, 93, 22, 74, 30), Arrays.asList(21, 2, 72, 11, 57),
+        List<List> resultListe2_2 = Arrays.asList(Arrays.asList(12, 81, 4),
+                Arrays.asList(5, 31, 17),
+                Arrays.asList(99));
+        List<List> resultListe2_3 = Arrays.asList(Arrays.asList(12, 81, 4, 5),
+                Arrays.asList(31, 17, 99));
+        List<List> resultListe3_1 = Arrays.asList(Arrays.asList(18, 93),
+                Arrays.asList(22, 74),
+                Arrays.asList(30, 21),
+                Arrays.asList(2, 72),
+                Arrays.asList(11, 57),
+                Arrays.asList(0));
+        List<List> resultListe3_2 = Arrays.asList(Arrays.asList(18, 93, 22),
+                Arrays.asList(74, 30, 21),
+                Arrays.asList(2, 72, 11),
+                Arrays.asList(57, 0));
+        List<List> resultListe3_3 = Arrays.asList(Arrays.asList(18, 93, 22, 74, 30),
+                Arrays.asList(21, 2, 72, 11, 57),
                 Arrays.asList(0));
         Assertions.assertEquals(Lists.partition(liste1, 2), resultListe1_1, "6 items splitted on 3 parts of 2 elements tests FAILED");
         Assertions.assertEquals(Lists.partition(liste1, 3), resultListe1_2, "6 items splitted on 2 parts of 3 elements tests FAILED");
@@ -42,6 +65,19 @@ class ListsTests {
         Assertions.assertEquals(Lists.partition(liste3, 2), resultListe3_1, "11 items splitted on 6 parts of 2 elements tests FAILED");
         Assertions.assertEquals(Lists.partition(liste3, 3), resultListe3_2, "11 items splitted on 4 parts of 3 elements tests FAILED");
         Assertions.assertEquals(Lists.partition(liste3, 5), resultListe3_3, "11 items splitted on 3 parts of 5 elements tests FAILED");
+
+    }
+
+    @Test
+    void partitionErrorTests() {
+    Throwable exception1 = Assertions.assertThrows(Exception.class, () -> Lists.partition(liste1, 7));
+    Throwable exception2 = Assertions.assertThrows(Exception.class, () -> Lists.partition(liste2, 0));
+    Throwable exception3 = Assertions.assertThrows(Exception.class, () -> Lists.partition(emptyList, 3));
+
+    Assertions.assertEquals(exception1.getMessage(), Lists.SMALLLISTERROR);
+    Assertions.assertEquals(exception2.getMessage(), Lists.NULLSIZEVALUE);
+    Assertions.assertEquals(exception3.getMessage(), Lists.EMPTYLISTERROR);
+
 
     }
 
